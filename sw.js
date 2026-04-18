2d757cd6cecb9d7b981ab49ea0b80abf0043e3b3
jodyashx-byte <jodyashx@gmail.com>
2026-04-18 18:23:09 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "calculator-v2";
+const CACHE = "calculator-v3";
 
 const ASSETS = [
   "./",
@@ -8,24 +8,28 @@ const ASSETS = [
   "./icon-512.png"
 ];
 
-self.addEventListener("install", e=>{
+self.addEventListener("install", e => {
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener("activate", e=>{
+self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k=>{
-        if(k!==CACHE) return caches.delete(k);
-      }))
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE) return caches.delete(k);
+        })
+      )
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", e=>{
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res=>res||fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
